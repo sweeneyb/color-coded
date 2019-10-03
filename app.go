f1977579c61fabefd6d8df7d63436985f037b801
sweeneyb <tab126@gmail.com>
2019-10-03 15:57:49 -0500

    silly change to launch the kick the CI
    silly change to launch the kick the CI

--- app.go
+++ app.go
@@ -19,7 +19,7 @@ func main() {
 	}  
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:" + os.Getenv("COLOR") + "'> Requested: %s\n </html>", r.URL.Path)
+		fmt.Fprintf(w, "<html onclick=\"window.location.href = '/die'\" style='background:" + os.Getenv("COLOR") + "'> FOO Requested: %s\n </html>", r.URL.Path)
 	})
 
 	http.HandleFunc("/dashboard", func(w http.ResponseWriter, r *http.Request) {
